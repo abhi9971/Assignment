0a3f28545c0714e18e656e2b0988d04f3b533479
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2021-11-29 18:28:26 +0530

    Added Forloop
    some imp question using for loop

---
+++ 6th.sh
@@ -0,0 +1,13 @@
+Write a program to compute Factors of a number N using prime factorization method.
+
+Logic -> Traverse till i*i <= N instead of i <= N for efficiency.
+O/P -> Print the prime factors of number N.
+
+#!/bin/bash
+
+echo "Enter num"
+read num
+for ((i=1; i<=1; i++))
+do 
+     echo "factor $num"
+done
